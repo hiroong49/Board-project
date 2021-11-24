edc74a306053f3f06edaf251bc11a330c73e349b
Jimeaning <kjimin63@naver.com>
2021-11-25 01:43:46 +0900

    게시판 목록 보기

---
+++ react-board-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Container from "react-bootstrap/Container";
+import BoardList from "./components/BoardList";
+
+function App() {
+  return (
+      <Container className="p-3">
+        <BoardList></BoardList>
+      </Container>
+  );
+}
+
+export default App;
---
+++ react-board-typescript/src/components/BoardList.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Col, Row} from "react-bootstrap";
+import {Board} from "../dto/Board";
+
+const BoardList: React.FC = () => {
+    const [boardList, setBoardList] = useState<Array<Board>>([]);
+
+    useEffect(() => {
+        getBoardList();
+    }, []);
+
+    const getBoardList = async () => {
+        // res는 http response의 header + body를 모두 갖고 있다.
+        const res  = await axios.get('/api/boards');
+        console.log(res);
+        setBoardList(res.data);
+    }
+    return (
+        <>
+            {
+                boardList.map((board: Board)=>
+                    <Row className="py-2 board">
+                        <Col>{board.title}</Col>
+                        <Col xs="auto" sm="auto">{board.created}</Col>
+                    </Row>)
+            }
+        </>
+    );
+};
+
+export default BoardList;
